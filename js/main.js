053580b7460edc9edd48a69416f0799b72e52539
Cory Spicer <corys4139@gmail.com>
2020-08-10 11:26:36 -0400

    buttons console.log

--- js/main.js
+++ js/main.js
@@ -8,16 +8,53 @@
 
 /*------Cached Element References------*/
 
+const yepBtn = document.getElementById("yep");
+const nopeBtn = document.getElementById("nope");
+const darkModeBtn = document.getElementById("darkMode");
+
+console.log(yepBtn)
 //div id profilePic
 //div id profileInfo
 
 /*------Event Listeners------*/
+
+yepBtn.addEventListener('click', ()=> {
+    console.log('yep');
+    getProfile();
+})
+
+nopeBtn.addEventListener('click', ()=>{
+    console.log('nope')
+    getProfile();
+})
+
+darkModeBtn.addEventListener('click', ()=>{
+    console.log('Dark mode engaged')
+})
 
 //listen for click on yep button that will store a profile in a "liked" folder, then reinitialize the program to send a new profile
 //listen for click on nope button, that reinitializes the program to send a new profile
 //listen for click on dark mode button that changes the color scheme
 
 /*------Functions------*/
+
+function test() {
+    console.log('test')
+}
+
+function getProfile(){
+    console.log('fetching...')
+    fetch("https:pipl.ir/v1/getPerson")
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
 
 //write a function that initializes the application: brings up a new profile that can be clicked on
 
