bce1436b82aa4bee7e04145e9b5b4616d8555401
Cory Spicer <corys4139@gmail.com>
2020-08-10 23:13:53 -0400

    adding profile info as cached elements

--- js/main.js
+++ js/main.js
@@ -24,20 +24,20 @@ const profilePicRequest = new Request ('http://cors-anywhere.herokuapp.com/thisp
 const yepBtn = document.getElementById("yep");
 const nopeBtn = document.getElementById("nope");
 const darkModeBtn = document.getElementById("darkMode");
-
-//div id profilePic
-//div id profileInfo
+const profilePicture = document.getElementById("profilePic");
+const profileInfo = document.getElementById("profileInfo");
+const container = document.getElementById("containerDiv");
 
 /*------Event Listeners------*/
 
 yepBtn.addEventListener('click', ()=> {
     getProfile();
-    getProfilePic();
+    // getProfilePic();
 })
 
 nopeBtn.addEventListener('click', ()=> {
     getProfile();
-    getProfilePic();
+    // getProfilePic();
 })
 
 darkModeBtn.addEventListener('click', ()=> {
@@ -57,16 +57,19 @@ function test() {
 function getProfile(){
     fetch("https://randomuser.me/api/")
     .then((response) => {
-        console.log(response)
         return response.json()
     })
     .then((data) => {
-        console.log(data.results[0])
+        let newProfile = {}
+        newProfile["name"] = data.results[0].name.first
+        newProfile["city"] = data.results[0].location.city
+        newProfile["age"] = data.results[0].dob.age
     })
     .catch((err) => {
         console.log(err)
     })
 }
+
 
 function getProfilePic (){
     fetch("http://thispersondoesnotexist.com")
@@ -79,8 +82,28 @@ function getProfilePic (){
     .catch((err) => {
         console.log(err)
     })
+    render()
 }
 
+function appendDiv(quote, artist) {
+        let newDiv = document.createElement("div")
+        newDiv.innerHTML = `<div class="card mb-3" style="max-width: 540px;">
+                                <div class="row no-gutters">
+                                    <div class="col-md-4">
+                                        <img src="..." class="card-img" alt="...">
+                                    </div>
+                                <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                                    </div>
+                                </div>
+                            </div>
+                            `
+        container.appendChild(newDiv)
+    }
 //write a function that initializes the application: brings up a new profile that can be clicked on
 
-//write a function that will return 'it's a match' occasionally when pressing the yep button, that asks if you want to send a message introducing yourself.
+//write a function that will return 'it's a match' occasionally when pressing the yep button, that asks if you want to send a message introducing yourself
